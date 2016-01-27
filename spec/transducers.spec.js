687505a9927b54e42d91ba19b94a1b5764e26a73
roschart <jlbalirac@hotmail.com>
2016-01-27 15:10:26 +0100

    Add some abstractiosn of filter and map

--- spec/transducers.spec.js
+++ spec/transducers.spec.js
@@ -6,7 +6,13 @@ function inc (x) { return x + 1 }
 
 function isEven (x) { return x % 2 === 0 }
 
-describe('A simple use of map and filter', () => {
+function mapReduce (f) {
+  return (list, x) => {
+    return list.concat([f(x)])
+  }
+}
+
+describe('A simple use of map, filter and reduce', () => {
   it('range generate a array', () => {
     expect([0, 1, 2]).toEqual(range(3))
   })
@@ -25,5 +31,27 @@ describe('A simple use of map and filter', () => {
       return list
     }
     expect([1, 2, 3]).toEqual(range(3).reduce(mapIncReducer, []))
+  })
+})
+
+describe('A general mapReduce abstraction can be used', () => {
+  it('mapReduce with inc', () => {
+    expect([1, 2, 3]).toEqual(range(3).reduce(mapReduce(inc), []))
+  })
+
+  it('mapReduce with anonimouos fucntions ', () => {
+    expect([0, 1, 4]).toEqual(range(3).reduce(mapReduce((x) => x * x), []))
+  })
+})
+
+describe('Filter can be implemented like reduce', () => {
+  function filterIsEvenReducer (list, x) {
+    if (x % 2 === 0) {
+      return list.concat([x])
+    }
+    return list
+  }
+  it('Filter Even whit reduce', () => {
+    expect([0, 2, 4]).toEqual(range(6).reduce(filterIsEvenReducer, []))
   })
 })
